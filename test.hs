0627bafb0ee083e21a4d50498935c770d23c798c
Chris <cspollard@gmail.com>
2014-02-03 14:38:45 -0500

    working on json.

---
+++ test.hs
@@ -0,0 +1,20 @@
+import Text.HandsomeSoup (openUrl)
+import Control.Monad.Maybe (runMaybeT)
+import Data.Aeson
+import Data.ByteString.Lazy.Char8 (pack)
+import Control.Applicative
+
+testhtml :: String
+testhtml = "http://www.ncaa.com/game/basketball-men/d1/2014/01/29/arizona-stanford/play-by-play"
+--testhtml = "http://www.google.com/search?q=egon+schiele"
+
+
+testjson :: String
+testjson = "http://data.ncaa.com/sites/default/files/data/game/basketball-men/d1/2014/01/29/arizona-stanford/pbp.json"
+
+main :: IO ()
+main = do
+    s <- runMaybeT $ openUrl testjson
+    print . decode . pack $ s
+
+    return ()
